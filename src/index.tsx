8bbad7c51adbef6454d854707694b9725f4e76d5
Raslav Milutinovic <admin@rasovica.com>
2020-11-15 20:31:11 +0100

    feat: Add sentry, better mobile support

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 import "./style/global.css";
 
 import { App } from "./App";
+
+Sentry.init({
+  dsn:
+    "https://5559fad401054db38a3eb6a9cf518f78@o477035.ingest.sentry.io/5517406",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
